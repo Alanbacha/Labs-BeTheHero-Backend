93cfd8ca8df96ca0f39a05ee9fa6357794e78c9d
Alan Bacha <alanbacha@gmail.com>
2020-03-27 18:25:08 -0400

    Adjusting port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const app = require("./app");
 
 // Setting the port where the application wil run
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || process.env.PORT_ENV || 3333;
 
 app.listen(port, () => {
 	console.log(`Server running at localhost:` + port);
